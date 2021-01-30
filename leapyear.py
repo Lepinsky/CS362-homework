cfb03bfb0ea354e643b8bc0e6f96120db7cc56e8
Jared L <44125943+Lepinsky@users.noreply.github.com>
2021-01-30 15:34:31 -0800

    Create leapyear.py

---
+++ leapyear.py
@@ -0,0 +1,12 @@
+year = input("Enter year: ")
+
+if year % 4 == 0:
+   if year % 100 == 0:
+      if year % 400 == 0:
+	 print("%d is a leap year." % (year))
+      else:
+	 print("%d is not a leap year." % (year))
+   else:
+      print("%d is a leap year." % (year))
+else:
+   print("%d is not a leap year." % (year))
