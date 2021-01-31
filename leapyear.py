2be81829dfc427ced8ab1dd2530579feaf248fcd
Jared L <44125943+Lepinsky@users.noreply.github.com>
2021-01-30 23:06:28 -0800

    Added case for year < 0

--- leapyear.py
+++ leapyear.py
@@ -1,14 +1,17 @@
 try:
     year = int(input("Enter year: "))
-    if year % 4 == 0:
-        if year % 100 == 0:
-            if year % 400 == 0:
-                print("%d is a leap year." % (year))
+    if year > 0:
+        if year % 4 == 0:
+            if year % 100 == 0:
+                if year % 400 == 0:
+                    print("%d is a leap year." % (year))
+                else:
+                    print("%d is not a leap year." % (year))
             else:
-                print("%d is not a leap year." % (year))
+                print("%d is a leap year." % (year))
         else:
-            print("%d is a leap year." % (year))
+            print("%d is not a leap year." % (year))
     else:
-        print("%d is not a leap year." % (year))
+        print("Error: Invalid year")
 except:
-    print("Error: Invalid year")
+    print("Error: Invalid Input")
