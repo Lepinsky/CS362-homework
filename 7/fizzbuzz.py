be9c9902d12bfdb4c98a5a65bdad0d65865b86ab
Jared L <44125943+Lepinsky@users.noreply.github.com>
2021-03-07 19:43:30 -0800

    Implemented TDD method
    Changed FizzBuzz function to return the value for the given number and the main code to call the function for range 1-100

--- 7/fizzbuzz.py
+++ 7/fizzbuzz.py
@@ -1,6 +1,6 @@
 def fizzbuzz(n): # Prints FizzBuzz for range 1 to n
-    for i in range(1,n):
-        print("Fizz"*(i%3==0)+"Buzz"*(i%5==0) or i)
+        return "Fizz"*(n%3==0)+"Buzz"*(n%5==0) or n
 
 if __name__ == "__main__":
-    fizzbuzz(100)
+        for i in range(1,101):
+            print(fizzbuzz(i))
